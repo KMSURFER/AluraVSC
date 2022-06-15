eeb28f9396f0509bae631e4701304942e2f30f6c
unknown <https://github.com/KMSURFER/AluraVSC.git>
2022-06-15 18:00:34 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// === -> simbolizam comparações entre as variaveis mostrando se são verdadeiras ou falsas exemplo : 
+// true -> verdadeiro 
+// false -> falso 
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
+
