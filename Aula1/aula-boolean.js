11d6a9aed93f9c3baceb79cae1748ddeb087506c
unknown <https://github.com/KMSURFER/AluraVSC.git>
2022-06-20 18:03:23 -0300

    codigos var e conversoes

---
+++ Aula1/aula-boolean.js
@@ -0,0 +1,49 @@
+// === -> simbolizam comparações entre as variaveis mostrando se são verdadeiras ou falsas exemplo : 
+// true -> verdadeiro 
+// false -> falso 
+
+//const primeiroNumero = 5;
+//const segundoNumero = 5;
+
+//console.log(primeiroNumero === segundoNumero);
+
+//const texto1 = "Alura";
+//const texto2 = "alura";
+
+//console.log(texto1 === texto2);
+
+//EXEMPLOS 
+
+//const usuarioLogado = true
+//const contaPaga = false
+
+// truthy ou falsy
+
+// 0 => false
+// 1 => true 
+
+//console.log(0 == false)
+//console.log("" == false)
+//console.log(1 == true)
+
+// undefined
+
+// null ==> vazio ou nada
+
+let minhaVar ;
+let varNull = null;
+
+console.log(minhaVar)
+console.log(varNull)
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
+
+
+
+
+
+
